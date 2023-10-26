0d812a693dfef547a8cf8d8094d74ef6bc50a35b
huynhthienba <ba.huynh@apps-cyclone.com>
2023-10-26 10:11:25 +0700

    feat: composion api setup

--- vuejs/money_tasks/src/pages/home/index.vue
+++ vuejs/money_tasks/src/pages/home/index.vue
@@ -1,14 +1,48 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<template lang="">
-  <div>
-    Overview page is here...
-  </div>
+<template>
+  <h1>Overview page is here...</h1>
+  <input type="text" placeholder="Search name..." v-model="searchTxt" />
+  <ul>
+    <li v-for="(customer, index) in customersFilter" :key="index">
+      {{ customer }}
+    </li>
+  </ul>
 </template>
 <script>
+import { computed, reactive, ref, watch, watchEffect } from "vue";
 export default {
-  
-}
+  props: {
+    theme: {
+      type: String,
+      default: "light",
+    },
+  },
+  // eslint-disable-next-line vue/no-setup-props-destructure
+  setup({ theme }, { emit }) {
+    console.log(theme);
+    console.log(emit);
+
+    const searchTxt = ref("");
+    const customers = reactive(["Tom", "Bob", "Marry", "Doe", "John"]);
+
+    const customersFilter = computed(() =>
+      customers
+        .map((customer) => customer.toLowerCase())
+        .filter((customer) => customer.includes(searchTxt.value.toLowerCase()))
+    );
+
+    watch(searchTxt, (newVal, oldVal) => console.log(newVal, oldVal));
+
+    watchEffect(() => {
+      if (searchTxt.value) {
+        console.log(searchTxt.value);
+      }
+    });
+
+    return {
+      searchTxt,
+      customersFilter,
+    };
+  },
+};
 </script>
-<style lang="">
-  
-</style>
