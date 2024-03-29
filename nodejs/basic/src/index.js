dd362744756318d717dd7cfb47ec824ce016bc80
thienba <macm3pro@Thien-Ba.local>
2024-03-29 23:58:41 +0700

    refactor: create routes, app/controller

--- nodejs/basic/src/index.js
+++ nodejs/basic/src/index.js
@@ -1,33 +1,26 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const { engine } = require('express-handlebars');
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const route = require("./routes");
 
-app.use(express.static(path.join(__dirname, './public/')));
+app.use(express.static(path.join(__dirname, "./public/")));
 
 // app.use(morgan('combined'));
 
-app.engine('hbs', engine({
-    extname: ".hbs"
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, './resources/views'));
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
 
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "./resources/views"));
 
-app.get('/', function (req, res) {
-    res.render('home');
-});
-
-app.get('/news', function (req, res) {
-    res.render('news');
-});
-
-app.get('/search', function (req, res) {
-    console.log("query: ", req.query.q);
-    res.render('search');
-});
+route(app);
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
